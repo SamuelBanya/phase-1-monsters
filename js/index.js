677ed2f1605b036ce2856058e85a664ea1705126
Samuel Banya <sbanya@fastmail.com>
2022-08-15 19:20:17 -0400

    Adding Heroku app's API endpoint to repo to replace localhost

--- js/index.js
+++ js/index.js
@@ -36,10 +36,10 @@ function createMonsterForm(e) {
   let descriptionInput = document.createElement("input");
   descriptionInput.setAttribute("placeholder", "description...");
   let submitButton = document.createElement("input");
-  submitButton.type = 'button';
-  submitButton.id = 'submit';
-  submitButton.value = 'Submit';
-  submitButton.className = 'btn';
+  submitButton.type = "button";
+  submitButton.id = "submit";
+  submitButton.value = "Submit";
+  submitButton.className = "btn";
 
   submitButton.addEventListener("click", (e) => {
     let monsterContainer = document.querySelector("#monster-container");
@@ -61,25 +61,28 @@ function createMonsterForm(e) {
     bioParagraph.textContent = "Bio: " + monsterDescription;
     let breakTag = document.createElement("br");
 
-    monsterContainer.append(monsterNameHeader, ageHeader, bioParagraph, breakTag);
+    monsterContainer.append(
+      monsterNameHeader,
+      ageHeader,
+      bioParagraph,
+      breakTag
+    );
 
-    fetch("http://localhost:3000/monsters", {
+    fetch("https://phase-1-monsters-jsonserver.herokuapp.com/monsters", {
       method: "POST",
       headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify ({
+      body: JSON.stringify({
         name: monsterName,
         age: monsterAge,
-        description: monsterDescription
-      })
+        description: monsterDescription,
+      }),
     })
       .then((response) => response.json())
-      .then((obj) => {
-      })
-      .catch((error) => {
-      });
+      .then((obj) => {})
+      .catch((error) => {});
   });
   form.append(nameInput, ageInput, descriptionInput, submitButton);
   createMonsterContainer.append(form);
@@ -88,7 +91,7 @@ function createMonsterForm(e) {
 function listMonsters(pageNumber) {
   let monsterContainer = document.querySelector("#monster-container");
   // Fetch request to determine total number of monsters on page:
-  fetch("http://localhost:3000/monsters", {
+  fetch("https://phase-1-monsters-jsonserver.herokuapp.com/monsters", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -102,12 +105,15 @@ function listMonsters(pageNumber) {
       console.log("error.message: ", error.message);
     });
   // Fetch request for 50 monsters per page:
-  fetch(`http://localhost:3000/monsters/?_limit=50&_page=${pageNumber}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
+  fetch(
+    `https://phase-1-monsters-jsonserver.herokuapp.com/monsters/?_limit=50&_page=${pageNumber}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
     .then((response) => response.json())
     .then((obj) => {
       obj.forEach((monsterObj) => {
@@ -144,23 +150,24 @@ function backButtonClick() {
     let monsterContainer = document.querySelector("#monster-container");
     monsterContainer.innerHTML = "";
     listMonsters(pageNumber);
-  }
-  else {
-    console.log("You have reached the first page, please use the forward button!");
+  } else {
+    console.log(
+      "You have reached the first page, please use the forward button!"
+    );
   }
 }
 
 function forwardButtonClick() {
   // Use .lastChildElement.id of the div of the last monster in the collection, if its less than the total amount in the collection, then continue
-  const lastElement = document.querySelector("#monster-container").lastElementChild.id;
+  const lastElement =
+    document.querySelector("#monster-container").lastElementChild.id;
 
   if (lastElement < totalMonsters) {
     pageNumber += 1;
     listMonsters(pageNumber);
     let monsterContainer = document.querySelector("#monster-container");
     monsterContainer.innerHTML = "";
-  }
-  else {
+  } else {
     console.log("You have created the final page, please use the back button!");
   }
 }
